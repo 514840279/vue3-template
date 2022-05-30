bd931df8fa451b7099fce28d61959731f02d0dac
王滕辉 <514840279@qq.com>
2022-05-30 13:10:58 +0800

    axios

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(VueAxios, axios)
 app.mount('#app')
