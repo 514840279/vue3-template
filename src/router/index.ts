c1391426ba71fa45531fb41eae5c18e647a59fa9
王滕辉 <514840279@qq.com>
2022-07-28 17:50:53 +0800

    createWebHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 
 // import Index from '../views/index.vue'
 // import Admin from '../views/Admin.vue'
@@ -80,7 +80,7 @@ const routes = [
 // 暂时保持简单
 export const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHashHistory(),
+  // history: createWebHashHistory(),
+  history: createWebHistory(),
   routes, // `routes: routes` 的缩写
 })
-
